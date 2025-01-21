f4924b50971967e7a3db2e12fe8fa31d4cd6994c
bobromero <rromero123102@gmail.com>
2025-01-20 20:53:23 -0500

    add transparent background

--- init.lua
+++ init.lua
@@ -882,6 +882,13 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
