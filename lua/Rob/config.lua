70381c7c485f8b1e32935fdc2a753ee8ba8aaef3
bobromero <rromero123102@gmail.com>
2025-02-13 16:50:25 -0500

    make relative line numbers white for readability

--- lua/Rob/config.lua
+++ lua/Rob/config.lua
@@ -1,6 +1,14 @@
 vim.opt.relativenumber = true
 
+vim.keymap.set('n', '<leader>e', ':Ex<CR>')
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white', bold = true })
+
 vim.keymap.set('i', '<C-s>', '<Esc> :LspRestart<CR> :w<CR>')
 vim.keymap.set('n', '<C-s>', ':LspRestart<CR> :w<CR>')
 vim.keymap.set('i', '<C-c>', '<Esc> :LspRestart<CR>')
-vim.keymap.set('n', '<leader>e', ':Ex<CR>')
