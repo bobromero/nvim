7ebcbd9f4d5d126c5249d7d337865cdbc863f346
bobromero <rromero123102@gmail.com>
2025-02-18 18:00:02 -0500

    changed comment color

--- lua/Rob/config.lua
+++ lua/Rob/config.lua
@@ -9,6 +9,9 @@ vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white', bold = true })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white', bold = true })
 
+vim.api.nvim_set_hl(0, 'Comment', { fg = '#a8a8a8' })
+vim.api.nvim_set_hl(0, '@comment', { link = 'Comment' })
+-- code
 vim.keymap.set('i', '<C-s>', '<Esc> :LspRestart<CR> :w<CR>')
 vim.keymap.set('n', '<C-s>', ':LspRestart<CR> :w<CR>')
 vim.keymap.set('i', '<C-c>', '<Esc> :LspRestart<CR>')
