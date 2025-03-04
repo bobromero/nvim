1128cea828daf13df56329d87773ac8d2ea7c348
bobromero <rromero123102@gmail.com>
2025-03-04 14:52:29 -0500

    create config and load plugins

--- lua/Rob/configs/themeconfig.lua
+++ lua/Rob/configs/themeconfig.lua
@@ -1,13 +1,10 @@
-require 'Rob.keymap'
-require 'Rob.colorscheme'
-vim.opt.relativenumber = true
-
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white', bold = true })
+vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'white', bold = true })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white', bold = true })
 
 vim.api.nvim_set_hl(0, 'Comment', { fg = '#a8a8a8' })
