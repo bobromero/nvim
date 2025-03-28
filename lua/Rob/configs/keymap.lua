339731703ae3fc2f15904c7e5fdc46c2b62b36f7
bobromero <rromero123102@gmail.com>
2025-03-28 15:09:48 -0400

    restored C-c to lsp restart

--- lua/Rob/configs/keymap.lua
+++ lua/Rob/configs/keymap.lua
@@ -9,6 +9,8 @@ vim.keymap.set('n', '<leader>k', '^', { desc = 'Move to the start of the line' }
 vim.keymap.set('v', '<leader>h', '0', { desc = 'Move to the beginning of the line' })
 vim.keymap.set('n', '<leader>h', '0', { desc = 'Move to the beginning of the line' })
 
+-- vim.keymap.set('i', '<C-c>', '<Esc> :LspRestart<CR>')
+
 vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
   expr = true,
   replace_keycodes = false,
