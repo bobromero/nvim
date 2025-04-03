6576e1363d882096c62ed1456758841f67af2fa3
bobromero <rromero123102@gmail.com>
2025-04-03 17:21:09 -0400

    update transparency

--- lua/Rob/plugins/colorscheme.lua
+++ lua/Rob/plugins/colorscheme.lua
@@ -2,10 +2,6 @@ return {
   'rebelot/kanagawa.nvim',
   opts = {
     transparent = true,
-    styles = {
-      sidebars = 'transparent',
-      floats = 'transparent',
-    },
   },
   priority = 1000, -- make sure to load this before all the other start plugins.
   init = function()
@@ -16,23 +12,3 @@ return {
     vim.cmd.hi 'Comment gui=none'
   end,
 }
-
--- tokyonight
--- return {
---   'folke/tokyonight.nvim',
---   opts = {
---     transparent = true,
---     styles = {
---       sidebars = 'transparent',
---       floats = 'transparent',
---     },
---   },
---   priority = 1000, -- make sure to load this before all the other start plugins.
---   init = function()
---     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
---     vim.cmd.colorscheme 'tokyonight-night'
---
---     -- You can configure highlights by doing something like:
---     vim.cmd.hi 'Comment gui=none'
---   end,
--- }
