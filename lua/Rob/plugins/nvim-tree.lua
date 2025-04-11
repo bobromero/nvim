abaf39e1aa31047b8e5df758c2e09af0cbba4235
bobromero <rromero123102@gmail.com>
2025-04-11 17:29:56 -0400

    added toggle transparency

--- lua/Rob/plugins/nvim-tree.lua
+++ lua/Rob/plugins/nvim-tree.lua
@@ -1,6 +1,14 @@
 return {
   'nvim-tree/nvim-tree.lua',
   version = '*',
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_gitignored = false,
+      },
+    },
+  },
   lazy = false,
   dependencies = {
     'nvim-tree/nvim-web-devicons',
