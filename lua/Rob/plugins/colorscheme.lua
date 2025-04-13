58761dcb1be338545b2da854322775dc0e90c564
bobromero <rromero123102@gmail.com>
2025-04-13 12:59:09 -0400

    transparency workaround

--- lua/Rob/plugins/colorscheme.lua
+++ lua/Rob/plugins/colorscheme.lua
@@ -9,8 +9,7 @@ return {
   },
   priority = 1000, -- make sure to load this before all the other start plugins.
   init = function()
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'kanagawa-dragon'
+    vim.cmd.colorscheme 'kanagawa'
 
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
